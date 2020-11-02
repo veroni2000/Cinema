47164cca276bf0b45eebf265f42545d03dc0b312
pavcata11 <35233727+pavcata11@users.noreply.github.com>
2020-11-02 11:40:17 +0200

    Update CinemaApplication.java

--- src/main/java/com/cats/cinema/CinemaApplication.java
+++ src/main/java/com/cats/cinema/CinemaApplication.java
@@ -7,7 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CinemaApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(CinemaApplication.class, args);
+		/// i need help
+		int a;
+		int b;
 	}
 
 }
