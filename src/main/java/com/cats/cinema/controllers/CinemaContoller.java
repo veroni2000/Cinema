3703e888e3605760869356769901312cc2d21ca0
veroni2000 <g7l1812@gmail.com>
2020-11-05 12:13:58 +0200

    Update CinemaContoller.java

--- src/main/java/com/cats/cinema/controllers/CinemaContoller.java
+++ src/main/java/com/cats/cinema/controllers/CinemaContoller.java
@@ -3,11 +3,13 @@ package com.cats.cinema.controllers;
 import com.cats.cinema.entities.Users;
 import com.cats.cinema.repositories.UsersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("cinema")
@@ -17,7 +19,39 @@ public class CinemaContoller {
     @GetMapping("/all")
     public List<Users> getUsers()
     {
+
         return  usersRepository.findAll();
     }
 
+    @GetMapping("/search/id")
+    public String getUserById(@RequestParam(required = false) Long id){
+        String user = null;
+        try{
+            user= usersRepository.findById(id).toString();
+        }catch (Exception i){
+            return i.getClass().getName();
+        }
+        return user;
+    }
+
+    @PostMapping("/save")
+    public ResponseEntity<?> saveOrUpdate(@RequestParam(required = false) String name,
+                                          @RequestParam(required = false) String email,
+                                          @RequestParam(required = false) String password,
+                                          @RequestParam(required = false)String role){
+        Users user = new Users(email,password,name,role);
+        user = usersRepository.save(user);
+        Map<String, Object> response = new HashMap<>();
+        response.put("geneartedId", user.getUser_id());
+        response.put("message","Успешно записан! ");
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    /*DeleteMapping("/delete")
+        public ResponseEntity<?> deleteUser(@RequestParam String name,
+                                            @RequestParam Long id){
+        //...
+            return ResponseEntity.ok("Успешно изтрит! ");
+        }*/
 }
