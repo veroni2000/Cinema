f32b299ef13b3cc499ce20328d4d99e3a93e23e3
chavdar00 <chavdargoranov00@gmail.com>
2021-01-04 19:13:15 +0200

    random stuff

--- cinema-ui/src/main.js
+++ cinema-ui/src/main.js
@@ -5,11 +5,13 @@ import store from './store'
 import * as axios from 'axios'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
-import { BTable, BPagination, NavbarPlugin, BCarousel, BCarouselSlide } from 'bootstrap-vue'
+import { BTable, BPagination, NavbarPlugin, BCarousel, BCarouselSlide, FormInputPlugin, BButton } from 'bootstrap-vue'
 
 Vue.component('b-pagination', BPagination)
 Vue.component('b-table', BTable)
+Vue.component('b-button', BButton)
 Vue.use(NavbarPlugin)
+Vue.use(FormInputPlugin)
 Vue.component('b-carousel-slide', BCarouselSlide)
 Vue.component('b-carousel', BCarousel)
 Vue.prototype.$axios = axios
