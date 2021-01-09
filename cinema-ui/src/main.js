3de84f11c834fbf708b89a003634fce52e795e45
pavcata11 <35233727+pavcata11@users.noreply.github.com>
2021-01-09 17:06:39 +0200

    Revert "home page"
    This reverts commit 6da42cd8bdcc1e2f89fc40b6e48e60ded4335359.

--- cinema-ui/src/main.js
+++ cinema-ui/src/main.js
@@ -5,7 +5,7 @@ import store from './store'
 import * as axios from 'axios'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
-import { BTable, BPagination, NavbarPlugin, BCarousel, BCarouselSlide, FormInputPlugin, BButton, BImg } from 'bootstrap-vue'
+import { BTable, BPagination, NavbarPlugin, BCarousel, BCarouselSlide, FormInputPlugin, BButton } from 'bootstrap-vue'
 Vue.component('b-pagination', BPagination)
 Vue.component('b-table', BTable)
 Vue.component('b-button', BButton)
@@ -13,7 +13,6 @@ Vue.use(NavbarPlugin)
 Vue.use(FormInputPlugin)
 Vue.component('b-carousel-slide', BCarouselSlide)
 Vue.component('b-carousel', BCarousel)
-Vue.component('b-img', BImg)
 Vue.prototype.$axios = axios
 Vue.config.productionTip = false
 
--- cinema-ui/src/views/Home.vue
+++ cinema-ui/src/views/Home.vue
@@ -1,32 +1,33 @@
+
 <template>
   <div class="home">
     <b-carousel
       id="carousel-1"
       v-model="slide"
-      :interval="5555"
+      :interval="4000"
       controls
       indicators
       background="#ababab"
-      img-width="1000"
-      img-height="300"
+      img-width="1024"
+      img-height="480"
       style="text-shadow: 1px 1px 2px #333;"
       @sliding-start="onSlideStart"
       @sliding-end="onSlideEnd"
     >
       <!-- Text slides with image -->
       <b-carousel-slide
-        caption="Кино Мегами"
-        text=""
-        img-src="https://images.unsplash.com/photo-1573727951373-ac2b41ed1400?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=2500&q=80"
+        caption="First slide"
+        text="Nulla vitae elit libero, a pharetra augue mollis interdum."
+        img-src="https://flickside.com/wp-content/uploads/2016/12/La-La-Land-movie-1024x300.jpg"
       ></b-carousel-slide>
 
       <!-- Slides with custom text -->
-      <b-carousel-slide img-src="http://www.mydailywallpaper.com/wallcat/space/bg_wall.jpg">
-        <h1></h1>
+      <b-carousel-slide img-src="https://cdn.racingnews.co/wp-content/uploads/2018/07/The-Last-Race-Short-Track-racing-Movie-1024x300.jpg">
+        <h1>Hello world!</h1>
       </b-carousel-slide>
 
       <!-- Slides with image only -->
-      <b-carousel-slide img-src="https://greenwichinlondon.com/wp-content/uploads/2013/03/movies-filmed-in-greenwich-slider.jpg"></b-carousel-slide>
+      <b-carousel-slide img-src="https://photos1.blogger.com/blogger/3109/1546/1600/vaginas.jpg"></b-carousel-slide>
 
       <!-- Slides with img slot -->
       <!-- Note the classes .d-block and .img-fluid to prevent browser default image alignment -->
@@ -34,9 +35,9 @@
         <template #img>
           <img
             class="d-block img-fluid w-100"
-            width="1000"
-            height="300"
-            src="https://www.remarkablehoneymoons.com/wp-content/uploads/tahiti-nui-sale-1000x200.jpg"
+            width="1024"
+            height="480"
+            src="https://www.thenerddaily.com/wp-content/uploads/2020/01/Disney-Classics-1-1024x300.png?x34602"
             alt="image slot"
           >
         </template>
@@ -44,24 +45,6 @@
 
       <!-- Slide with blank fluid image to maintain slide aspect ratio -->
     </b-carousel>
-    <template>
-      <h5 class="p-5"></h5>
-      <p> <img src="https://mmtvmusic.com/wp-content/uploads/2019/12/parvia-bulgarski-kino-salon-mmtv-f.jpg"  width="450" height="222" style="float:left"> Първите кинопрожекции в България са се състояли в началото на декември 1896 г. в София, от професор Мелинсон в салона на хотел „Македония“ и кафене „България“. Според вестник „МИР“ са прожектирани „пет хубави картини из живота, предадени много добре от киноматографа“.
-        Доскоро се знаеше, че първата кинопрожекция се е състояла през 1897 г. в заведението на Марин Чолаков в град Русе. Собственик на кинематографическата машина и организатор на събитието бил Георги Кузмич. Той прожектирал филм, който показвал посрещането на цар Николай в Париж, един от парижките площади и движещ се влак. Месец по-късно, на 22 март, била организирана поредната прожекция в София.</p>
-      <h5 class="p-5"></h5>
-      <h5 class="p-5"></h5>
-      <p><img src="https://static.dir.bg/uploads/images/2020/01/12/1937276/768x432.jpg?_=1578852318"  width="450" height="220" style="float:right"> Комедията „Българан е галант е първият български игрален филм[1]. Прожектиран е за първи път на 13 януари 1915 г. и съобразно технологиите в киното по това време, филмът е черно-бял и „ням“. Сценарист, режисьор и изпълнител на главната роля е Васил Гендов. Смята се, че филмът е загубен и са останали едва 1 – 2 кадъра от него.</p>
-      <h5 class="p-5"></h5>
-      <h5 class="p-5"></h5>
-      <p><img src="https://scifi.bg/wp-content/uploads/2017/02/cinema.jpg"  width="900" height="300" style="vertical-align:middle"></p>
-      <p>
-        Кино Мегами е най-новото кино в България. Предлага последните филми на пазара и на достъпни цени. Качестовото е просто съвършено няма друго такова
-      </p>
-      <h5 class="p-5"></h5>
-      <nav class="navbar navbar-dark bg-dark">
-        <a class="navbar-brand" href="#"> Go up</a>
-      </nav>
-    </template>
   </div>
 </template>
 <script>
