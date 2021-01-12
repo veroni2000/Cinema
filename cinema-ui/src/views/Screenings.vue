bc02ae14e497d8dc541acd21f81d627b588ea544
veroni2000 <g7l1812@gmail.com>
2021-01-12 21:58:44 +0200

    screenings scedule

--- cinema-ui/src/views/Screenings.vue
+++ cinema-ui/src/views/Screenings.vue
@@ -1,21 +1,64 @@
+<style>
+.screeningsTable {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+  text-align: center;
+}
+.screeningsTable th{
+  border: 1px solid #2c3e50;
+  padding: 10px;
+  width: 150px;
+  background-color: #2c3e50;
+  color: white;
+}
+.screeningsTable td{
+  width: 150px;
+  height: 150px;
+  text-align: center;
+  padding: 10px;
+}
+.screeningsTable .rl{
+  color: #2c3e50;
+  background-color: white;
+  border: 2px solid #2c3e50;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  margin-bottom: 10px;
+  padding-top: 8px;
+}
+.screeningsTable .rl:hover{
+  background-color: #2c3e50;
+  color: white;
+  text-decoration: none;
+}
+</style>
 <template>
   <div>
-    <b-table
-      id = "screeningsTable"
-      striped
-      hover
-      :items="screenings"
-      :fields="fields"
-    >
-      <template v-for="field in fields" v-slot:[`cell(${field.key})`]="data">
-<!--        <div v-if="data.item.movie.title" v-bind:key="field">-->
-          <div  v-bind:key="field" :v-if="(data.item.time | moment('ddd D.MM.YYYY')) === (field.key)">
-              {{data.item.movie.title}}<p></p>
-              {{data.item.time | moment('ddd D.MM.YYYY')}}
+    <table class="screeningsTable">
+      <template>
+      <tr>
+        <span v-bind:key="field" v-for="field in fields">
+        <th v-bind:key="field">{{field.label}}</th>
+        </span>
+       </tr>
+        <span v-bind:key="field" v-for="field in fields">
+        <td>
+          <div v-bind:key="screening" v-for="screening in screenings">
+          <tr v-bind:key="screening" v-if="( moment(screening.time).format('ddd D.MM.YYYY') === (field.key))">
+           <router-link class="rl" :to="{name: 'ScreeningTab', params:{ id: screening.screening_id }}">
+            <p> {{screening.movie.title}}
+             {{screening.time | moment('H:mm')}}
+            </p>
+             </router-link>
+          </tr>
           </div>
-<!--        </div>-->
+        </td>
+        </span>
       </template>
-    </b-table>
+    </table>
+
   </div>
 </template>
 
@@ -35,6 +78,8 @@ export default {
         }],
         time: ''
       }],
+      date1: '',
+      date2: '',
       fields: [
         { key: day1, label: day1.toString() },
         { key: day2, label: day2.toString() },
@@ -46,8 +91,10 @@ export default {
       ]
     }
   },
+  // result: this.getScreeningsByDate(new Date(day1), new Date(day2))
   mounted () {
     this.getFutureScreenings()
+    // this.getScreeningsByDate(this.date1, this.date2)
   },
   methods: {
     getFutureScreenings () {
@@ -56,8 +103,11 @@ export default {
           this.screenings = response.data
         })
     },
-    isToday (date) {
-      return moment(date).isSame(moment().clone().startOf('day'), 'd')
+    getScreeningsByDate (date1, date2) {
+      ScreeningsService.getScreeningsByDate(date1, date2)
+        .then(response => {
+          this.screenings = response.data
+        })
     }
   },
   formatter: (value, key, item) => {
