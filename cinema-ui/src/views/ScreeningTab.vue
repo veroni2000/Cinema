7b3cf7e5d32c9362a2fc28d72b5d5e228b08ccc5
chavdar00 <chavdargoranov00@gmail.com>
2021-01-13 20:20:07 +0200

    screeningstab

--- cinema-ui/src/views/ScreeningTab.vue
+++ cinema-ui/src/views/ScreeningTab.vue
@@ -1,10 +1,59 @@
+<style>
+.mC{
+  width: 41%;
+  height: 500px;
+  margin-left: 28%;
+  background-color: #42b983;
+  border: 1px solid green;
+  position: center;
+}
+.sImg{
+  margin-top: -3%;
+  position: fixed;
+}
+.sTittle{
+  margin-top: 3%;
+  text-align: left;
+  font-size: 40px;
+  margin-left: 2%;
+  font-family: Arial;
+}
+.Ti{
+  margin-top: 25%;
+  font-family: Arial;
+}
+</style>
 <template>
 <div>
-  {{Screening.seats}}
-</div>
-  <div class="mycontainer">
+  <div class="mC">
+    <div class="sTittle">
+      {{Screening.movie.title}}
+    </div>
+    <img class="sImg" :src="'/pictures/' + Screening.movie.picture.toString()" width="200" >
+    <p class="Ti">
+      Закопуване на билет за фила {{Screening.movie.title}}, чиято прожекция ще бъде излъчена на {{Screening.time | moment('D.MM.YYYY')}} в {{Screening.time | moment('H:mm')}}
+    </p>
+    <b-form>
+      <b-row>
+      <b-col sm="9" style="margin-left: 13%">
+      <b-form-input
+        placeholder="Email"
+        size="lg"
+      >
+      </b-form-input>
+      </b-col><br>
+        <b-col sm="2" style="margin-left: 20%">
+          Брой билети
+          <b-form-input
+            size="lg"
+          >
+          </b-form-input>
+        </b-col>
+      </b-row>
+    </b-form>
 
   </div>
+</div>
 </template>
 
 <script>
@@ -42,11 +91,3 @@ export default {
   }
 }
 </script>
-<style>
-.mycontainer{
-  width: 33%;
-  height: 33%;
-  background-color: #42b983;
-  position: center;
-}
-</style>
